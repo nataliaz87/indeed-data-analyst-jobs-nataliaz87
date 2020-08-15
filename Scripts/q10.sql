e803a4099f3ddea9fb7bafd05bcd124c362d8a27
Natalia Zemchikhina <natalulik@hotmail.com>
2020-08-15 12:18:05 -0500

    Updated Q10 script

--- Scripts/q10.sql
+++ Scripts/q10.sql
@@ -1,5 +1,5 @@
-/*11.	11.	Find all the job titles that contain the word ‘Analyst’. How many different job titles are there? */
+/*10.	Add the code to order the query in #9 from highest to lowest average star rating.
+Which company with more than 5000 reviews across all locations in the dataset has the 
+highest star rating? What is that rating? */
 
-select title 
-from data_analyst_jobs 
-where title like 'Analyst%'
+select company, avg(star_rating) as avg_rat from data_analyst_jobs where review_count >5000 group by data_analyst_jobs.company order by avg_rat desc
